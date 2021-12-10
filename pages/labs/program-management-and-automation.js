e1efeda5dc190ef47dd175e00a87dc790cabffa9
Kurt Kemple <kurtiskemple@gmail.com>
2021-12-10 16:28:49 -0500

    more page updates

--- pages/labs/program-management-and-automation.js
+++ pages/labs/program-management-and-automation.js
@@ -1,17 +1,56 @@
-import styles from "@styles/page.module.css";
+import Head from "next/head";
+
+import pStyles from "@styles/page.module.css";
+import spStyles from "@styles/services-page.module.css";
 import SectionDivider from "@components/SectionDivider";
 
 export default function ProgramManagementAutomation() {
+  const styles = {
+    ...pStyles,
+    ...spStyles,
+  };
+
+  const title = "Labs / Program Management and Automation / Forthright";
+  const description =
+    "Launching is the easy part. In this workshop, you'll learn how to manage and scale your initiatives without sacrificing your time.";
+
   return (
     <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="og:title" content={title} />
+        <meta name="og:description" content={description} />
+      </Head>
       <div className={styles.service}>
         <h1>Program Management & Automation</h1>
-        <p className={styles.slogan}>
-          Launching is the easy part. In this workshop, you'll learn how to
-          manage and scale your initiatives without sacrificing your time.
-        </p>
+        <p className={styles.slogan}>{description}</p>
       </div>
       <SectionDivider />
+      <div className={styles.service}>
+        <h2 className={styles.howItWorksTitle}>What You'll Learn</h2>
+        <div className={styles.howItWorks}>
+          <div className={styles.step}>
+            <h3>Process Management</h3>
+            <p>
+              Never miss a beat. Learn how to use tools like Airtable to
+              organize and track your programs and initiatives so you can keep
+              them running smoothly without a significant time investment.
+              You'll also learn how to
+            </p>
+          </div>
+          <div className={styles.step}>
+            <h3>Automation</h3>
+            <p>
+              Leverage the power of low/no-code so you can automate away the
+              glue work for your programs and initiatives. Learn about
+              automation tools like Zapier, Pipedream, and GitHub Actions and
+              how to put them into action with workflows like event
+              registration, tracking community engagement, and more.
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
